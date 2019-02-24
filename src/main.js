45f943a4af6a60f8308941335406ef661ae0c715
akhodas <a.l.khodas@gmail.com>
2019-02-24 14:55:50 +0300

    add webpack webpack-dev-server eslint and create empty test module

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import createFilter from './create-filter';
+import createEvent from './create-event';
+
+// eslint-disable-next-line no-console
+console.log(`hello people`);
+createFilter(`hi-hi-hi`);
+createEvent(`HI-yhu`);
